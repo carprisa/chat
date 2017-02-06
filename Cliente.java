1dfec042b3cf5e1fbc563fdce463d56f7e2dac80
Carlos Prieto <carlosprieto@gmail.com>
2017-02-07 00:52:00 +0100

    first commit

---
+++ Cliente.java
@@ -0,0 +1,92 @@
+package carlos.sockets;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+
+
+class MarcoCliente extends JFrame{  // es el marco de la ventana
+	
+	public MarcoCliente(){
+		
+		setBounds(600,300,200,350);  // establece los limites
+		
+		LaminaMarcoCliente milamina = new LaminaMarcoCliente();
+		
+		add(milamina);
+		
+		setVisible(true);
+				
+	}
+}
+
+class LaminaMarcoCliente extends JPanel{
+	
+	private JTextField campo1;
+	private JButton miboton;
+	
+	public LaminaMarcoCliente(){
+		
+		JLabel texto = new JLabel("CLIENTE");
+		
+		add(texto); //añade a JPanel
+		
+		campo1 = new JTextField(20);
+		
+		add(campo1);
+		
+		miboton = new JButton("Enviar");
+		
+		EnviaTexto mievento = new EnviaTexto(); // se crea el evento
+		
+		miboton.addActionListener(mievento);	// se ejecuta la acción
+		
+		add(miboton);
+	}
+	
+	private class EnviaTexto implements ActionListener{   // se crea un evento que reacciones a la pulsación de un botón
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+			//System.out.println(campo1.getText());  Para que al pulsar en el botón aparezcan los datos del campo de texto campo1
+			
+			try {
+				Socket misocket = new Socket("192.168.1.13", 9999);
+				
+			} catch (UnknownHostException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				System.out.println(e1.getMessage());
+			}
+			
+			
+		}
+		
+	}
+}
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		
+		MarcoCliente mimarco = new MarcoCliente();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // para que salga al cerrar
+		
+	}
+	
+}
